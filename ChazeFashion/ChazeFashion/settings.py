673767d8824ee759a7e503d8f456c9841938968e
Ayush Kumar <122884120+AYUSH09874@users.noreply.github.com>
2025-07-23 10:31:40 +0530

    Update settings.py

--- ChazeFashion/ChazeFashion/settings.py
+++ ChazeFashion/ChazeFashion/settings.py
@@ -109,3 +109,6 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+
