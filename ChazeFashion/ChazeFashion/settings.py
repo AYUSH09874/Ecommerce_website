fd97f3d42cc97480cbdd8e91a59bdd0125f1386d
Ayush Kumar <122884120+AYUSH09874@users.noreply.github.com>
2025-07-23 10:28:58 +0530

    Update settings.py

--- ChazeFashion/ChazeFashion/settings.py
+++ ChazeFashion/ChazeFashion/settings.py
@@ -76,7 +76,7 @@ USE_TZ = True
 
 # Static files
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / "static"]
+STATICFILES_DIRS = [BASE_DIR / "ChazeFashion" / "static"]
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # ✅ WhiteNoise settings
